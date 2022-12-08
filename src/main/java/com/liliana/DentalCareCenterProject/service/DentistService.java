84d022173f253568b0f86d229effcb7575d71de2
LilianaFranco <franco0123@gmail.com>
2022-12-07 19:55:23 -0500

    Log4j2 is working now

--- src/main/java/com/liliana/DentalCareCenterProject/service/DentistService.java
+++ src/main/java/com/liliana/DentalCareCenterProject/service/DentistService.java
@@ -5,6 +5,8 @@ import com.liliana.DentalCareCenterProject.exception.ResourceNotFoundException;
 import com.liliana.DentalCareCenterProject.model.Dentist;
 import com.liliana.DentalCareCenterProject.model.DentistDto;
 import com.liliana.DentalCareCenterProject.repository.DentistRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ import java.util.Set;
 
 @Service
 public class DentistService implements InterfaceDentistService{
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     //Dependency Injection
     @Autowired
@@ -42,6 +46,7 @@ public class DentistService implements InterfaceDentistService{
         DentistDto dentistDto = null;
         //If it exists, convert to dentistDto
         if(dentist.isEmpty()){
+            LOGGER.error("Dentist not found");
             throw new ResourceNotFoundException("Dentist not found");
         }else{
             dentistDto = mapper.convertValue(dentist, DentistDto.class);
